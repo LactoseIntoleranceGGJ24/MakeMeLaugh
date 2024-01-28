c7d0e1a1355a46d4e2011dbaf32677601ce6ef6e
Venvoid <157861158+Venvoid@users.noreply.github.com>
2024-01-28 14:18:02 +1100

    final probably hopefully

--- Assets/Scripts/LocationManagerScript.cs
+++ Assets/Scripts/LocationManagerScript.cs
@@ -30,6 +30,43 @@ public class LocationManagerScript : MonoBehaviour
         }
     }
 
+<<<<<<< Updated upstream
+=======
+    void LocationReached(float timeRemaining)
+    {
+        audioData.Play(0);
+        _activeLocation.GetComponent<SpriteRenderer>().color = Color.white;
+        int temp = 0;
+        
+        if (_questsRemaining >= 0 && _questsRemaining < 3) //delivery
+        {
+            locationNumber = Random.Range(0, _locationList.Length); //there is a small chance to get the same location twice in a row
+            _activeLocation = _locationList[locationNumber];
+            _reviewScript.GetComponent<ReviewScript>().Review(timeRemaining);
+            temp = -1;
+        }
+        if (_questsRemaining == 0) //final delivery
+        {
+            _activeLocation = _restaurant;
+            //_questsRemaining = 4;
+            temp = 3;
+        
+        }
+        if (_questsRemaining == 3) //restaurant
+        {
+            _player.GetComponent<PlayerHealth>().Heal(70);
+            _enemySpawner.GetComponent<EnemySpawner>().IncreaseSpawnRate();
+            locationNumber = Random.Range(0, _locationList.Length);
+            _activeLocation = _locationList[locationNumber];
+            temp = -1;
+        }
+
+        _activeLocation.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 0.2f) ;
+        arrowScript.UpdateLocation(_activeLocation);
+        _timer = 30f;
+        _questsRemaining += temp;
+    }
+>>>>>>> Stashed changes
 
 
 }
